22cb1dc2762d111cc27044ebb020d8e13f26d02c
vijay marka <111419184+DeveloperEducology@users.noreply.github.com>
2025-08-05 11:19:46 +0530

    Update Class2_maths.js

--- src/data/Topics/Class2_maths.js
+++ src/data/Topics/Class2_maths.js
@@ -1,53 +1,174 @@
 export const class2_maths_topicList = {
-  "A.1 Skip-Counting-by-pictures": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.2 Skip-counting sequences": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.2.1 missing number in sequence": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.2.2 Counting patterns-up to 100": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.3 Counting patterns - up to 100": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.4 Number lines - up to 100": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.5 Hundred chart": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.6 Even or odd": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.7 Identify numbers as even or odd": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.8 Select even or odd numbers": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.9 Even or odd numbers on number lines": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.10 When even or odd numbers come before or after?": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.11 Skip-counting stories": { "category": "Counting and Number Patterns", "grade": "2" },
-  "A.12 Skip-counting puzzles": { "category": "Counting and Number Patterns", "grade": "2" },
+  "A.1 Skip-Counting-by-pictures": {
+    category: "Counting and Number Patterns",
+    grade: "2",
+  },
+  "A.2 Skip-counting sequences": {
+    category: "Counting and Number Patterns",
+    grade: "2",
+  },
+  "A.2.1 missing number in sequence": {
+    category: "Counting and Number Patterns",
+    grade: "2",
+  },
+  "A.2.2 Counting patterns-up to 100": {
+    category: "Counting and Number Patterns",
+    grade: "2",
+  },
+  "A.3 Counting patterns - up to 100": {
+    category: "Counting and Number Patterns",
+    grade: "2",
+  },
+  "A.4 Number lines - up to 100": {
+    category: "Counting and Number Patterns",
+    grade: "2",
+  },
+  "A.5 Hundred chart": { category: "Counting and Number Patterns", grade: "2" },
+  "A.6 Even or odd": { category: "Counting and Number Patterns", grade: "2" },
+  "A.7 Identify numbers as even or odd": {
+    category: "Counting and Number Patterns",
+    grade: "2",
+  },
+  "A.8 Select even or odd numbers": {
+    category: "Counting and Number Patterns",
+    grade: "2",
+  },
+  "A.9 Even or odd numbers on number lines": {
+    category: "Counting and Number Patterns",
+    grade: "2",
+  },
+  "A.10 When even or odd numbers come before or after?": {
+    category: "Counting and Number Patterns",
+    grade: "2",
+  },
+  "A.11 Skip-counting stories": {
+    category: "Counting and Number Patterns",
+    grade: "2",
+  },
+  "A.12 Skip-counting puzzles": {
+    category: "Counting and Number Patterns",
+    grade: "2",
+  },
 
-  "B.1 Comparing numbers up to 100": { "category": "Comparing and Ordering", "grade": "2" },
-  "B.2 Put numbers up to 100 in order": { "category": "Comparing and Ordering", "grade": "2" },
-  "B.3 Greatest and least - word problems - up to 100": { "category": "Comparing and Ordering", "grade": "2" },
+  "B.1 Comparing numbers up to 100": {
+    category: "Comparing and Ordering",
+    grade: "2",
+  },
+  "B.2 Put numbers up to 100 in order": {
+    category: "Comparing and Ordering",
+    grade: "2",
+  },
+  "B.3 Greatest and least - word problems - up to 100": {
+    category: "Comparing and Ordering",
+    grade: "2",
+  },
 
-  "C.1 Ordinal numbers up to 10th": { "category": "Names of Numbers", "grade": "2" },
-  "C.2 Cardinal numbers up to 100": { "category": "Names of Numbers", "grade": "2" },
-  "C.3 Writing numbers up to 100 in words": { "category": "Names of Numbers", "grade": "2" },
-  "C.4 Distinguishing ordinal and cardinal numbers": { "category": "Names of Numbers", "grade": "2" },
+  "C.1 Ordinal numbers up to 10th": {
+    category: "Names of Numbers",
+    grade: "2",
+  },
+  "C.2 Cardinal numbers up to 100": {
+    category: "Names of Numbers",
+    grade: "2",
+  },
+  "C.3 Writing numbers up to 100 in words": {
+    category: "Names of Numbers",
+    grade: "2",
+  },
+  "C.4 Distinguishing ordinal and cardinal numbers": {
+    category: "Names of Numbers",
+    grade: "2",
+  },
 
-  "D.1 Growing patterns": { "category": "Patterns", "grade": "2" },
-  "D.2 Repeating patterns": { "category": "Patterns", "grade": "2" },
-  "D.3 Identify the next shape in a pattern": { "category": "Patterns", "grade": "2" },
-  "D.4 Number patterns - up to 100": { "category": "Patterns", "grade": "2" },
+  "D.1 Growing patterns": { category: "Patterns", grade: "2" },
+  "D.2 Repeating patterns": { category: "Patterns", grade: "2" },
+  "D.3 Identify the next shape in a pattern": {
+    category: "Patterns",
+    grade: "2",
+  },
+  "D.4 Number patterns - up to 100": { category: "Patterns", grade: "2" },
 
-  "E.1 Add with pictures - sums to 10": { "category": "Addition - One Digit", "grade": "2" },
-  "E.2 Add with pictures - sums to 20": { "category": "Addition - One Digit", "grade": "2" },
-  "E.3 Addition sentences - sums to 10": { "category": "Addition - One Digit", "grade": "2" },
-  "E.4 Add doubles": { "category": "Addition - One Digit", "grade": "2" },
-  "E.5 Add one-digit numbers": { "category": "Addition - One Digit", "grade": "2" },
-  "E.6 Addition with pictures - sums to 20": { "category": "Addition - One Digit", "grade": "2" },
-  "E.7 Write addition sentences to describe pictures - sums to 20": { "category": "Addition - One Digit", "grade": "2" },
-  "E.8 Addition input/output tables - sums to 20": { "category": "Addition - One Digit", "grade": "2" },
-  "E.9 Add zero": { "category": "Addition - One Digit", "grade": "2" },
-  "E.10 Addition word problems - one digit": { "category": "Addition - One Digit", "grade": "2" },
-  "E.11 Complete the addition sentence - one digit": { "category": "Addition - One Digit", "grade": "2" },
-  "E.12 Write the addition sentence - one digit": { "category": "Addition - One Digit", "grade": "2" },
-  "E.13 Balance addition equations - one digit": { "category": "Addition - One Digit", "grade": "2" },
-  "E.14 Add three or more one-digit numbers": { "category": "Addition - One Digit", "grade": "2" },
-  "E.15 Add three or more one-digit numbers - word problems": { "category": "Addition - One Digit", "grade": "2" },
-  "E.16 Identify repeated addition for arrays - sums to 10": { "category": "Addition - One Digit", "grade": "2" },
-  "E.17 Write addition sentences for arrays - sums to 10": { "category": "Addition - One Digit", "grade": "2" },
-  "E.18 Identify repeated addition for arrays - sums to 25": { "category": "Addition - One Digit", "grade": "2" },
-  "E.19 Write addition sentences for arrays - sums to 25": { "category": "Addition - One Digit", "grade": "2" },
-  "A.1 Tense Fill-in-the-Blank": { "category": "English", "grade": "2" },
-  "A.2 Synonym Match": { "category": "English", "grade": "2" },
-  "A.3 Reading Comprehension (True/False)": { "category": "English", "grade": "2" },
-}
+  "E.1 Add with pictures - sums to 10": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.2 Add with pictures - sums to 20": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.3 Addition sentences - sums to 10": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.4 Add doubles": { category: "Addition - One Digit", grade: "2" },
+  "E.5 Add one-digit numbers": { category: "Addition - One Digit", grade: "2" },
+  "E.6 Addition with pictures - sums to 20": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.7 Write addition sentences to describe pictures - sums to 20": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.8 Addition input/output tables - sums to 20": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.9 Add zero": { category: "Addition - One Digit", grade: "2" },
+  "E.10 Addition word problems - one digit": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.11 Complete the addition sentence - one digit": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.12 Write the addition sentence - one digit": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.13 Balance addition equations - one digit": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.14 Add three or more one-digit numbers": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.15 Add three or more one-digit numbers - word problems": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.16 Identify repeated addition for arrays - sums to 10": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.17 Write addition sentences for arrays - sums to 10": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.18 Identify repeated addition for arrays - sums to 25": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "E.19 Write addition sentences for arrays - sums to 25": {
+    category: "Addition - One Digit",
+    grade: "2",
+  },
+  "A.1 Tense Fill-in-the-Blank": { category: "English", grade: "2" },
+  "A.2 Synonym Match": { category: "English", grade: "2" },
+  "A.3 Reading Comprehension (True/False)": { category: "English", grade: "2" },
+  "N.1 numberSystem": { category: "Navodaya", grade: "5" },
+  "N.2 operationsOnWholeNumbers": { category: "Navodaya", grade: "5" },
+  "N.3 factorsAndMultiples": { category: "Navodaya", grade: "5" },
+  "A.1 fractionBasicsUnderstanding": {
+    category: "Navodaya-fractions",
+    grade: "5",
+  },
+  "A.2 ProperFractionQuestion": { category: "Navodaya-fractions", grade: "5" },
+  "ImproperToMixedQuestion": { category: "Navodaya-fractions", grade: "5" },
+  "MixedToImproperQuestion": { category: "Navodaya-fractions", grade: "5" },
+    "N.4 fractionAscendingOrder": { category: "Navodaya-fractions", grade: "5" },
+  "N.4a compareTwoFractions": { category: "Navodaya-fractions", grade: "5" },
+  "N.4b TwoFractionsDescending": { category: "Navodaya-fractions", grade: "2" },
+};
